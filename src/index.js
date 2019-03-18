37b0999cc72ac5c15ee6611764f7674cc45cb4f9
Anabfmv <anastasia.yarashukbfmv@gmail.com>
2019-03-18 10:43:45 +0300

    task: money-exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+module.exports = 
+function makeExchange(currency) {
+    let obj = {"H":0,"Q":0,"D":0,"N":0,"P":0};
+  
+    if(currency>10000){
+        obj.error ="You are rich, my friend! We don't have so much coins for exchange"
+     }
+    else {for( ;currency>0;){
+    
+        if(currency>= 50){
+            currency=currency-50;
+            obj.H+=1;
+            continue;
+        }
+        else if(currency>= 25){
+            currency=currency-25;
+            obj.Q+=1;
+            continue;
+        }
+        else if(currency>= 10){
+            currency=currency-10;
+            obj.D+=1;
+            continue;
+        }
+        else if(currency>= 5){
+            currency=currency-5;
+            obj.N+=1;
+            continue;
+        }
+        else if(currency>= 1){
+            currency=currency-1;
+            obj.P+=1;
+            continue;
+        }
+        }}
+
+        for(key in obj){
+            if(obj[key]==0){
+                delete obj[key];
+            }
+        }
+        return obj;
+    }
+
+
+
